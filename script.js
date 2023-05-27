24fc9ca0528eff06ab4301c9f3de01a13be41026
Alwyn <38840937+alwyn-t@users.noreply.github.com>
2023-05-26 23:53:45 -0700

    May 26 - Update of version 5
    Moved sections to be vertically aligned
    Navigation bar has been updated to be slightly transparent, smaller font size, remove number, changed mobile nav bar to take up the full screen and centred the text
    Updated the project blocks to have six rather than five blocks to be symmetrical

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onresize = snapToID;
-window.onload = snapToID;
+window.onresize = snapToId;
+window.onload = updateIdToUrl;
 
-function snapToID(){
+function snapToId(){
     if(document.getElementById('p0').checked) {
         window.location='#landing-page';
     }else if(document.getElementById('p1').checked) {
@@ -16,5 +16,34 @@ function snapToID(){
         window.location='#landing-page';
     }else if(document.getElementById('p6').checked) {
         window.location='#landing-page';
+    }else if(document.getElementById('p7').checked) {
+        window.location='#contact-page';
     }
+}
+
+function updateIdToUrl(){
+    let url = window.location.href;
+    let url_split = url.split('#');
+    console.log('loaded url: ' + url);
+    if(url_split.length > 1){
+        console.log('loaded id: ' + url_split[1]);
+        if(url_split[1] == "landing-page"){
+            document.getElementById('p0').checked = true;
+        }else if(url_split[1] == "landing-page"){
+            document.getElementById('p1').checked = true;
+        }else if(url_split[1] == "circle-page"){
+            document.getElementById('p2').checked = true;
+        }else if(url_split[1] == "chess-page"){
+            document.getElementById('p3').checked = true;
+        }else if(url_split[1] == "-page"){
+            document.getElementById('p4').checked = true;
+        }else if(url_split[1] == "-page"){
+            document.getElementById('p5').checked = true;
+        }else if(url_split[1] == "-page"){
+            document.getElementById('p6').checked = true;
+        }else if(url_split[1] == "contact-page"){
+            document.getElementById('p7').checked = true;
+        }
+    }
+    snapToId;
 }
